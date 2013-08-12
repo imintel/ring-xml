f1dd285fb8b57da75133713dab35ad21469c3dd6
Rocco Nicosia <funk3ns7ein@gmail.com>
2013-08-12 15:07:08 -0400

    V0.0.1. Now how do I get this thing on Clojars?

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject ring/ring-xml "0.0.1" 
+  :description "Ring middleware for handling XML."
+  :url "https://github.com"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [ring/ring-core "1.2.0"]])
