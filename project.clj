011817d516d0619b71f1f67a9630831f403cabc7
devstopfix <james@devstopfix.net>
2015-12-06 17:25:26 +0000

    Catch invalid XML and return 400 Bad Request
    Catch SAX exceptions and return 400.
    Bump Clojure and Ring versions.
    New test case.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject imintel/ring-xml "0.0.2" 
+(defproject imintel/ring-xml "0.0.3" 
   :description "Ring middleware for handling XML."
   :url "https://github.com/imintel/ring-xml"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [ring/ring-core "1.2.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [ring/ring-core "1.4.0"]])
